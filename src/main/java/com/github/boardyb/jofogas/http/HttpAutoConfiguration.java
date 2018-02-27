e77e30f0f1a3be9a8d94ff2ea77804da70561d78
mongooseasd <mongooseasd@gmail.com>
2018-02-27 12:30:16 +0100

    Created spring bean of socket factory and connection manager for http client.

--- src/main/java/com/github/boardyb/jofogas/http/HttpAutoConfiguration.java
+++ src/main/java/com/github/boardyb/jofogas/http/HttpAutoConfiguration.java
@@ -1,12 +1,20 @@
 package com.github.boardyb.jofogas.http;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static com.github.boardyb.jofogas.http.HttpClientConfigProvider.createConnectionManager;
-import static com.github.boardyb.jofogas.http.HttpClientConfigProvider.createSocketFactory;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import java.security.SecureRandom;
 
 @Configuration
 public class HttpAutoConfiguration {
@@ -14,9 +22,30 @@ public class HttpAutoConfiguration {
     @Bean
     public HttpClient httpClient() throws Exception {
         return HttpClients.custom()
-                .setSSLSocketFactory(createSocketFactory())
-                .setConnectionManager(createConnectionManager())
+                .setSSLSocketFactory(sslConnectionSocketFactory())
+                .setConnectionManager(connectionManager())
                 .build();
+    }
+
+    @Bean
+    public SSLConnectionSocketFactory sslConnectionSocketFactory() throws Exception {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(new KeyManager[0], new TrustManager[]{new AcceptAllTrustManager()}, new SecureRandom());
+        return new SSLConnectionSocketFactory(sslContext,
+                new String[]{"TLSv1",
+                        "SSLv3"},
+                null,
+                SSLConnectionSocketFactory
+                        .getDefaultHostnameVerifier());
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager connectionManager() throws Exception {
+        Registry<ConnectionSocketFactory> socketFactoryRegistry =
+                RegistryBuilder.<ConnectionSocketFactory>create().register("http", PlainConnectionSocketFactory.INSTANCE)
+                        .register("https", sslConnectionSocketFactory())
+                        .build();
+        return new PoolingHttpClientConnectionManager(socketFactoryRegistry);
     }
 
 }
